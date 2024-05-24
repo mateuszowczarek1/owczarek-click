00ee106c2f3e56ba22e3f5def3bbd820bfb304e1
Mateusz Owczarek <mateuszowczarek@onet.eu>
2024-05-24 09:22:13 +0200

    started working on a template

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import Particles from '@tsparticles/vue3';
+import { loadFull } from 'tsparticles';
+import App from './App.vue';
+import './style.css';
+import { Engine } from '@tsparticles/engine';
+
+
+createApp(App).use(Particles, {
+    init: async (engine: Engine) => {
+        await loadFull(engine as Engine); 
+    },
+}).mount('#app');
