6fd115ebf8b07c6476101d5eb30b9c7936642413
mateuszowczarek1 <157053173+mateuszowczarek1@users.noreply.github.com>
2024-05-27 09:06:43 +0200

    switched the resume files

--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -6,8 +6,8 @@ import SecondaryButton from './SecondaryButton.vue';
     <div class="rounded-xl border px-4 py-2 gap-4">
             <div class="font-semibold text-xl">Download my resume</div>
             <div class="flex flex-wrap mt-2 gap-2">
-              <SecondaryButton href="cv_eng_2024.pdf" name="ENG" />
-              <SecondaryButton href="cv_eng_2024.pdf" name="PL" />
+              <SecondaryButton href="cv_eng_2024_no_info.pdf" name="ENG" />
+              <SecondaryButton href="cv_pl_2024_no_info.pdf" name="PL" />
             </div>
           </div>
 </template>
